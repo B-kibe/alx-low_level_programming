b33e41754b0847bf9f6e7eaba3546866571e6dcf
B-kibe <briankones7@gmail.com>
2023-03-16 14:56:06 -0700

    c 6

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - prints a string to stdout
+ * Return: 0 if it is successful
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
