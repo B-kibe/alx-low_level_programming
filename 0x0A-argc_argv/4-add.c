7f4a10269ffc9a712a7c146c9ee9a1d2c6f76681
B-kibe <briankones7@gmail.com>
2023-04-10 19:26:40 -0700

    comm 2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,6 +8,7 @@
 	 *
 	 * Return: Always 0 (Success)
 	 */
+
 	int check_num(char *str)
 	{
 		/*Declaring variables*/
@@ -39,9 +40,7 @@
 	 * Return: Always 0 (Success)
 	 */
 
-
 	int main(int argc, char *argv[])
-
 
 	{
 
@@ -57,12 +56,10 @@
 		{
 			if (check_num(argv[count]))
 
-
 			{
 				str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
 				sum += str_to_int;
 			}
-
 
 			/*Condition if one of the number contains symbols that are not digits*/
 			else
@@ -76,9 +73,7 @@
 		}
 
 
-		printf("%d\n", sum); /*print sum*/
+printf("%d\n", sum); /*print sum*/
 
-
-		return (0);
-	}
-
+return (0);
+}
